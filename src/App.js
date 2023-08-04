850581304d730ffd764ccc9f367e9fbd19688a30
Shubham maurya <49495079+Ermaurya@users.noreply.github.com>
2023-08-04 08:31:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import JSX_001 from './components/JSX_001';
+import JSX_002 from './components/JSX_002';
+
+function App() {
+  return (
+    <>
+    <JSX_001/>
+     <JSX_002/>
+   
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/JSX_002.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const JSX_002 = () => {
+  return (
+    <div>
+      <div className='bg'>
+      <div className='text1'>
+        <h1 >The Evolution of <br/> Airbnb's Frontend</h1>
+        <h3>@spikebrehm</h3>
+        <h4>Airbnb</h4>
+      </div>
+      </div> 
+    </div>
+  )
+}
+
+export default JSX_002
